4c4c2018f0a9859fdf2d85a1515018cda26838ea
Jacob Beneski <jacobbeneski@gmail.com>
2020-05-23 15:23:20 -0500

    feat: Refactor container to use css grid.
    Use CSS Grid instead of Bulma's columns to allow for better flexibility & less boilerplate.

--- src/web/frontend/public/App.vue
+++ src/web/frontend/public/App.vue
@@ -4,7 +4,7 @@
     <nav class="breadcrumb boxBreadcrumbs" aria-label="breadcrumbs">
       <ul>
         <li>
-          <a href="#"
+          <a href="/"
             ><font-awesome-icon icon="home"></font-awesome-icon>&nbsp;Home</a
           >
         </li>
@@ -17,34 +17,32 @@
       </ul>
     </nav>
     <hr />
-    <div class="container">
-      <div class="columns">
-        <div v-for="n in 4" :key="n" class="column">
-          <div class="card">
-            <div class="card-content">
-              <p class="title">
-                “There are two hard things in computer science: cache
-                invalidation, naming things, and off-by-one errors.”
-              </p>
-              <p class="subtitle">
-                Jeff Atwood
-              </p>
-            </div>
-            <footer class="card-footer">
-              <p class="card-footer-item">
-                <span>
-                  View on
-                  <a
-                    href="https://twitter.com/codinghorror/status/506010907021828096"
-                    >Twitter</a
-                  >
-                </span>
-              </p>
-              <p class="card-footer-item">
-                <span> Share on <a href="#">Facebook</a> </span>
-              </p>
-            </footer>
+    <div class="container" :class="this.layout">
+      <div v-for="n in 35" :key="n">
+        <div class="card">
+          <div class="card-content">
+            <p class="title">
+              “There are two hard things in computer science: cache
+              invalidation, naming things, and off-by-one errors.”
+            </p>
+            <p class="subtitle">
+              Jeff Atwood
+            </p>
           </div>
+          <footer class="card-footer" v-if="layout === 'full'">
+            <p class="card-footer-item">
+              <span>
+                View on
+                <a
+                  href="https://twitter.com/codinghorror/status/506010907021828096"
+                  >Twitter</a
+                >
+              </span>
+            </p>
+            <p class="card-footer-item">
+              <span> Share on <a href="#">Facebook</a> </span>
+            </p>
+          </footer>
         </div>
       </div>
     </div>
@@ -70,6 +68,27 @@ export default class App extends Vue {
 
 <style lang="scss">
 @import "./style/_base.scss";
+.container {
+  display: grid;
+  gap: 1rem;
+  padding-bottom: 2rem;
+}
+
+.compact {
+  grid-template-columns: repeat(auto-fit, minmax(15vw, 1fr));
+  .card-content p {
+    font-size: 1rem;
+  }
+}
+
+.full {
+  grid-template-columns: repeat(auto-fit, minmax(25vw, 1fr));
+}
+
+.table {
+  grid-template-columns: 1fr;
+}
+
 .boxBreadcrumbs {
   padding: 20px 0px 0px 10px;
   margin-bottom: 0px;
