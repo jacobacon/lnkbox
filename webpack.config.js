069362c1763d47acab86807be4d1626bcf95dbb6
Jacob Beneski <jacobbeneski@gmail.com>
2020-06-01 23:30:49 -0500

    chore: Add HTML Webpack Plugin

--- webpack.config.js
+++ webpack.config.js
@@ -7,14 +7,16 @@ const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
 
 const TerserPlugin = require("terser-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-  entry: "./src/web/frontend/public/index.ts",
+  entry: ["./src/web/frontend/public/index.ts"],
   mode: process.env.NODE_ENV || "production",
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         loader: "ts-loader",
+        include: path.resolve(__dirname, "src"),
         exclude: "/node_modules/",
         options: {
           appendTsSuffixTo: [/\.vue$/],
@@ -22,6 +24,7 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
+        include: path.resolve(__dirname, "src"),
         loader: "file-loader",
         options: {
           name: "[name].[ext]",
@@ -30,14 +33,17 @@ module.exports = {
       },
       {
         test: /\.vue$/,
+        include: path.resolve(__dirname, "src"),
         use: "vue-loader",
       },
       {
         test: /\.css$/,
+        include: path.resolve(__dirname, "src"),
         use: ["style-loader", "css-loader"],
       },
       {
         test: /\.s[ac]ss$/i,
+        include: path.resolve(__dirname, "src"),
         use: [
           // Creates `style` nodes from JS strings
           "style-loader",
@@ -59,17 +65,29 @@ module.exports = {
     },
   },
   optimization: {
-    minimize: true,
+    minimize: process.env.NODE_ENV === "production",
     minimizer: [new TerserPlugin()],
   },
+  devServer: {
+    publicPath: "/",
+  },
+  devtool: "inline-source-map",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist/web/frontend/public"),
+    publicPath: "/",
   },
   plugins: [
-    //new BundleAnalyzerPlugin(),
     new CleanWebpackPlugin(),
+    new BundleAnalyzerPlugin({
+      analyzerMode:
+        process.env.NODE_ENV === "development" ? "server" : "disabled",
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/web/frontend/index.template.html"),
+    }),
     new CompressionPlugin(),
+    /*
     new CopyPlugin([
       {
         from: path.resolve(__dirname, "src/web/frontend/public"),
@@ -77,6 +95,7 @@ module.exports = {
         ignore: ["*.ts", "*.scss", "*.vue"],
       },
     ]),
+    */
     new VueLoaderPlugin(),
   ],
 };
