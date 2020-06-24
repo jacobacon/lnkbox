4cc50b6346213473b75e694d673e7673ad912faf
Jacob Beneski <jacobbeneski@gmail.com>
2020-06-23 22:06:53 -0500

    feat: Load entry from API on EntryPage load

--- src/web/frontend/public/components/EntryPage.vue
+++ src/web/frontend/public/components/EntryPage.vue
@@ -1,13 +1,36 @@
 <template>
-  <div>
-    Hello! {{ $route.path }}
-    <Entry></Entry>
+  <div class="container">
+    {{ entry }}
+    <span style="color: red">{{ $route.path }}</span>
+    <Entry v-if="entry" :entry="entry"></Entry>
+    <div v-if="loading" style="color: red">Loading...</div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Prop } from "vue-property-decorator";
 import Entry from "./Entry.vue";
+import EntryType from "../../../../common/interfaces/entry";
 @Component({ components: { Entry } })
-export default class EntryPage extends Vue {}
+export default class EntryPage extends Vue {
+  loading: boolean = true;
+  entry: any;
+
+  created() {
+    this.getEntry();
+  }
+
+  async getEntry() {
+    this.axios
+      .get(`/api/entries/${this.$route.path}`)
+      .then(resp => {
+        this.loading = false;
+
+        this.entry = resp.data.response.data;
+      })
+      .catch(err => {
+        alert(err);
+      });
+  }
+}
 </script>
