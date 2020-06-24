44c1cc0bee9f6c9eefab4b642b1f7737f48df44a
Jacob Beneski <jacobbeneski@gmail.com>
2020-06-24 14:09:38 -0500

    feat: Load chil entries on page change

--- src/web/frontend/public/components/EntryPage.vue
+++ src/web/frontend/public/components/EntryPage.vue
@@ -1,28 +1,41 @@
 <template>
   <div class="container">
-    {{ entry }}
-    <span style="color: red">{{ $route.path }}</span>
-    <Entry v-if="entry" :entry="entry"></Entry>
+    <div v-if="entry" class="level">
+      <h1 class="title is-1">{{entry.title}}</h1>
+    </div>
+
+    <hr />
     <div v-if="loading" style="color: red">Loading...</div>
+    <div class="folder-contents">
+      <div v-for="entry in childEntries" :key="entry.$loki">
+        <Entry :entry="entry"></Entry>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue, Prop } from "vue-property-decorator";
+import { Component, Vue, Prop, Watch } from "vue-property-decorator";
 import Entry from "./Entry.vue";
 import EntryType from "../../../../common/interfaces/entry";
 @Component({ components: { Entry } })
 export default class EntryPage extends Vue {
   loading: boolean = true;
-  entry: any;
+  entry: any = null;
+  folderID: number = null;
+
+  childEntries: [any] = null;
 
   created() {
+    this.folderID = this.getEntryID();
     this.getEntry();
+    this.getChildren();
   }
 
+  @Watch("$route")
   async getEntry() {
     this.axios
-      .get(`/api/entries/${this.$route.path}`)
+      .get(`/api/entries/${this.getEntryID()}`)
       .then(resp => {
         this.loading = false;
 
@@ -32,5 +45,29 @@ export default class EntryPage extends Vue {
         alert(err);
       });
   }
+
+  @Watch("$route")
+  async getChildren() {
+    this.axios
+      .get(`/api/entries?parentID=${this.getEntryID()}`)
+      .then(resp => {
+        this.childEntries = resp.data.response.data;
+      })
+      .catch(err => {
+        alert(err);
+      });
+  }
+
+  getEntryID(): number {
+    return Number(this.$route.path.split("/").slice(-1)[0]);
+  }
 }
 </script>
+
+<style lang="scss">
+.folder-contents {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 1em;
+}
+</style>
